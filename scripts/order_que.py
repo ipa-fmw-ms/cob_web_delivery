7ea27350c7b11360c29c5f9a18d22e4722ad216b
ipa-fmw-ms <max-sieber@gmx.de>
2014-12-02 18:04:10 +0100

    service dummy

--- scripts/order_que.py
+++ scripts/order_que.py
@@ -4,8 +4,10 @@ from cob_web_delivery.srv import *
 import rospy
 
 def handle_order_que(req):
-    print "Appending %s with Priority %u to Que %s "%(req.item, req.priority)
-    return OrderQueResponse(true)
+    d = rospy.Duration.from_sec(7.1)
+    rospy.sleep(d)
+    print "Appending %s with Priority %d and destination x: %F y: %F to Que  "%(req.item, req.priority, req.target_pose1.position.x, req.target_pose1.position.y)
+    return OrderQueResponse(True)
 
 def order_que_server():
     rospy.init_node('order_que')
