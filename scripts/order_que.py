a0c6f69540622fb0d5660152a0284c81428ff9d1
fmw-ms <max-sieber@gmx.de>
2015-01-06 03:02:43 +0100

    get room from segmentation

--- scripts/order_que.py
+++ scripts/order_que.py
@@ -12,19 +12,33 @@ class QueServer:
     self.ac = actionlib.SimpleActionClient('delivery', DeliveryAction)
     print "Waiting for delivery Actions Sevrer"
     self.ac.wait_for_server()
+    print "Waiting for segmentation data"
+    self.mapsegs = rospy.get_param('segs') #try wait
+    self.destinations = rospy.get_param('destinations')
     print "Ready to Accept Orders"
     
   def handle_order_que(self, req):
+      room_id = self.get_room(pixels=req.pixels, mapsegs=self.mapsegs)
+      if room_id == (0 or 255):
+        accepted = False
+      target = self.get_destinations(room_id=room_id)
+      print target
       print "Appending %s with Priority %d and destination x: %F y: %F to Que  "%(req.item, req.priority, req.target_pose1.position.x, req.target_pose1.position.y)
-      rospy.sleep(rospy.Duration.from_sec(1.0))
       goal = DeliveryGoal()
       goal.target_pose1 = req.target_pose1
       goal.target_pose2 = req.target_pose2
       goal.item = req.item
       self.ac.send_goal(goal)#feedbackCB in here?
-      done = self.ac.wait_for_result()
+      accepted = self.ac.wait_for_result()
       print "Action Returned Error: %s in state: %d "%(self.ac.get_result().Error, self.ac.get_result().state)
-      return OrderQueResponse(done)
+      return OrderQueResponse(accepted, "Room 1", req.target_pose1, req.target_pose2)
+
+  def get_room(self, pixels, mapsegs):
+      pixfield = mapsegs["width"]*(pixels[1]-1)+(pixels[0]-1)
+      return mapsegs["data"][pixfield]
+
+  def get_destinations(self, room_id):
+      return self.destinations["room_position_dict"][str(room_id)]
 
 if __name__ == "__main__":
     rospy.init_node('order_que')
