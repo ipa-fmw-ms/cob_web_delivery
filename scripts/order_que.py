a772cd27f540cfa44895004458410ef2bafa2bef
fmw-ms <max-sieber@gmx.de>
2015-04-13 15:10:01 +0200

    add PriorityQueue

--- scripts/order_que.py
+++ scripts/order_que.py
@@ -7,10 +7,18 @@ from geometry_msgs.msg import Pose, Pose2D, Point, Quaternion, Twist
 import rospy
 import actionlib
 
+from Queue import PriorityQueue #todo: customize
+import threading
+
 
 class QueServer:
     def __init__(self):
         rospy.on_shutdown(self.shutdown)
+
+        self.pq = PriorityQueue()
+        self.t = threading.Thread(target=self.send_que)
+        self.t.start()
+
         self.s = rospy.Service('order_que', OrderQue, self.handle_order_que)
         self.ac = actionlib.SimpleActionClient('delivery', DeliveryAction)
         print "Waiting for segmentation data"
@@ -18,6 +26,7 @@ class QueServer:
         print "Waiting for delivery Actions Server"
         self.ac.wait_for_server()
         print "Ready to Accept Orders \n"
+
 
     def handle_order_que(self, req):
         servback = OrderQueResponse()
@@ -30,9 +39,9 @@ class QueServer:
             servback.room = "no Room"
             return servback
         pickups = self.assign_poses(self.get_param_by_id(param="pickup_positions", param_id=req.item))
-        print "pickup assign", pickups
+        #print "pickup assign", pickups
         targets = self.assign_poses(self.get_param_by_id(param="destinations", param_id=room_id))
-        print "targets assign", targets
+        #print "targets assign", targets
         # Todo: append clicked pose
 
         goal = DeliveryGoal()
@@ -40,12 +49,31 @@ class QueServer:
         goal.destinations = targets
         goal.pickup_poses = pickups
 
-        self.ac.send_goal(goal)  # feedbackCB in here?
+        self.pq.put((req.priority, goal)) #append goal
 
-        accepted = self.ac.wait_for_result()
+
+        #self.ac.send_goal(goal)  # feedbackCB in here?
+
+        #accepted = self.ac.wait_for_result()
         #print "Action Returned Error: %s in state: %d " % (self.ac.get_result().Error, self.ac.get_result().state)
-        print "action returned", self.ac.get_result()
-        print "\n"
+        #print "action returned", self.ac.get_result()
+        #print "\n"
+
+    def send_que(self):
+        print "send que thread started"
+        while True:
+            try:
+                goal = self.pq.get()
+                print goal[1].item
+                self.ac.send_goal(goal[1])
+                self.ac.wait_for_result()
+                print "action returned", self.ac.get_result()
+            except:
+
+                print "no new goals"
+                pass
+            rospy.sleep(rospy.Duration.from_sec(0.5))
+
 
     def assign_poses(self, pose_list):
         array = []
@@ -71,6 +99,7 @@ class QueServer:
         print "\n shutdown order que"
         self.ac.cancel_all_goals
         self.s.shutdown()
+        #self.t.stop()
 
 
 if __name__ == "__main__":
